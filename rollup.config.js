12d59cb3ed5f3bebc73e0658a9670d9bff7caa4e
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-07-12 10:04:54 -0400

    New dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
 import bowerResolve from 'rollup-plugin-bower-resolve';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
@@ -10,23 +11,29 @@ export default {
         babel({
             exclude: [
                 'bower_components/lodash/**',
-                'bower_components/jQuery/**',
+                'bower_components/jquery/**',
                 'node_modules/**'
                 ]
         }),
         bowerResolve({
             skip: ['rdfstore'],
             override: {
-                jQuery: 'dist/jquery.js',
-                lodash: 'dist/lodash.js'
+                jquery: 'dist/jquery.js',
+                lodash: 'dist/lodash.js',
+                'typeahead.js': 'dist/typeahead.bundle.js'
             }
+        }),
+        nodeResolve({
+            jsnext:true,
+            main:true,
+            skip:['rdfstore']
         }),
         commonjs()
     ],
     globals: {
         rdfstore: 'rdfstore'
     },
-    moduleName: 'annotatorPlugin',
+    moduleName: 'perseids',
     targets: [
         {
             dest: 'build/js/annotator.js',
