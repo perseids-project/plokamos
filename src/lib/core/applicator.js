8ca206e3be470bd5b95fef23d0f43adcef06a07e
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-07-12 10:07:30 -0400

    Updated Applicator

--- src/lib/core/applicator.js
+++ src/lib/core/applicator.js
@@ -1,32 +1,48 @@
-import $ from 'jQuery'
+import $ from 'jquery'
+import TextQuoteAnchor from 'dom-anchor-text-quote'
 
 // I have a list of selector types
 // I have a list of queries to get selector data
 // I have a list of functions to apply
 class Applicator {
     
-    constructor() {
+    constructor () {
         this.escape = (s) => s.replace(/[-/\\^$*+?.()（）|[\]{}]/g, '\\$&').replace(/\$/g, '$$$$');
-        this.apply = {
-            "http://www.w3.org/na/oa#TextQuoteSelector": (selector, triple) => {
-                var prefix = selector.prefix ? this.escape(selector.prefix.value).replace(/ /g,'\\s*') : ''
-                var exact = selector.exact ? this.escape(selector.exact.value).replace(/ /g,'\\s*') : ''
-                var suffix = selector.suffix ? this.escape(selector.suffix.value).replace(/ /g,'\\s*') : ''
-                var ex = [prefix+'\\s*',exact,'\\s*'+suffix].map((x) => '('+x+')').join('')
-                var rx = new RegExp(ex,"m")
-                var ml = rx.exec($('article').text())
-                if (ml) $(`:contains('${ml[0]}')`).html(function (i, o) {
-                    return o.replace(rx, "$1<span data-desc='undefined'>$2</span>$3");
+        this.graph = {
+            "http://www.w3.org/ns/oa#hasBody": (id) => [
+                "SELECT ?id ?subject ?predicate ?object ?graph",
+                "WHERE {",
+                "GRAPH ?g {?id <http://www.w3.org/ns/oa#hasBody> ?graph } .",
+                "GRAPH ?graph {?subject ?predicate ?object}",
+                "}"
+            ].join("\n")
+        }
+        this.mark = {
+            "http://www.w3.org/ns/oa#TextQuoteSelector": (selector, triple) => {
+                var prefix = selector.prefix ? selector.prefix.value : ''
+                var exact = selector.exact ? selector.exact.value : ''
+                var suffix = selector.suffix ? selector.suffix.value : ''
+                // covering for trimmed pre-/suffixes
+                prefix = prefix[-1]===' ' ? prefix : prefix+' '
+                suffix = [',','.',';',':'].indexOf(suffix[0])+1 ? suffix : ' '+suffix
+                $(`:contains('${prefix+exact+suffix}')`).last().html(function (i, o) {
+                    // TODO: REPLACE IS STILL TO SLOW [much better with last()]
+                    // TODO: NOT ROBUST IN CASE OF N-1 : ANNOTATIONS-TOKEN
+                    // TODO: -> USE TEXTQUOTEANNOTATOR ?
+                    return o.replace(
+                        prefix+exact+suffix,
+                        `${prefix}<span class ='perseids-annotation' id='${selector.id.value}'>${exact}</span>${suffix}`
+                    );
                 })
-                else console.log(ex); // Let's see what's the issue!
             }
         };
         this.selectors = {
-            "http://www.w3.org/na/oa#TextQuoteSelector": (id) => [
+            "http://www.w3.org/ns/oa#TextQuoteSelector": (id) => [
                 "SELECT ?id ?prefix ?exact ?suffix",
                 "WHERE {",
                 "GRAPH ?g {",
-                `${id || "?id"} <http://www.w3.org/ns/oa#hasSelector> ?selector .`,
+                `${id || "?id"} <http://www.w3.org/ns/oa#hasTarget> ?target .`,
+                "?target <http://www.w3.org/ns/oa#hasSelector> ?selector .",
                 "?selector <http://www.w3.org/ns/oa#prefix> ?prefix .",
                 "?selector <http://www.w3.org/ns/oa#exact> ?exact .",
                 "?selector <http://www.w3.org/ns/oa#suffix> ?suffix .",
@@ -35,9 +51,23 @@ class Applicator {
         };
     }
 
-    annotate (model, id)  {
-        model.execute(this.selectors["http://www.w3.org/na/oa#TextQuoteSelector"](id))
-            .then((data) => _.map(data).map((x) => this.apply["http://www.w3.org/na/oa#TextQuoteSelector"](x)))
+    load (model, id)  {
+            // get TextSelectors
+        model.execute(this.selectors["http://www.w3.org/ns/oa#TextQuoteSelector"](id))
+            // mark positions in HTML
+            .then((data) => {
+                data.map((x) => this.mark["http://www.w3.org/ns/oa#TextQuoteSelector"](x))
+            })
+            // get triples
+            .then((data) => model.execute(this.graph["http://www.w3.org/ns/oa#hasBody"](id)))
+
+            .then((data) => data.forEach((x) => {
+                var element = $(document.getElementById(x.id.value))
+                var array = element.data(x.graph.value) || []
+                    element.data(x.graph.value,_.concat(array,{subject:x.subject.value, predicate:x.predicate.value,object:x.object.value}))
+            }))
+            .then((data) => $.getScript('js/marginotes.js'))
+            .then((data) => $('.perseids-annotation').marginotes({field:'id'})) // map data or get perseids-annotation class ?
     }
     
 }
