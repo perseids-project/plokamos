9a2bd6d46ed93b9b8a883edd417d42b4bbddfeae
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-07-12 10:10:12 -0400

    Updated dependencies & architecture

--- src/main.js
+++ src/main.js
@@ -1,35 +1,47 @@
-import $ from 'jQuery'
-import jQuery from 'jQuery'
+import $ from 'jquery'
+import jquery from 'jquery'
 import _ from 'lodash'
 import Model from './lib/core/model.js'
 import Applicator from './lib/core/applicator.js'
+import Annotator from './lib/core/annotator.js'
+import TextQuoteAnchor from 'dom-anchor-text-quote'
 window.$ = $
-window.jQuery = jQuery
+window.jQuery = jquery
+window.jquery = jquery
 window._ = _
+import 'typeahead.js'
 
-var getEndpoint = () => $('#annotator-main').data().sparqlEndpoint
-var getUrn = () => $('#annotator-main').data().urn
-var getUser = () => undefined // $('#annotator-main').dataset.user
+
+var applicator = new Applicator();
+var annotator = new Annotator();
 
 var initialize = () => {
+    var getEndpoint = () => $('#annotator-main').data().sparqlEndpoint
+    var getUrn = () => $('#annotator-main').data().urn
+    var getUser = () => undefined // $('#annotator-main').dataset.user
     var model = new Model();
-    var applicator = new Applicator();
     var results = model
-        .load(getEndpoint(), getUrn(),getUser())
-        .then( (sucess) => applicator.annotate(model) )
+        .load( getEndpoint(), getUrn(),getUser() )
+        .then( (success) => applicator.load(model) )
+        .then( (success) => annotator.load(model,getUrn()) )
     
     // have document with urn, config
     // get urn & endpoint
     // query endpoint for urn annotations
     // insert annotations into rdfstore
-    
-    // TODO: retrieve from rdfstore and
-    // TODO: create annotation markers in document
-    // TODO: show controls to create new annotations
-    // TODO:
-    return [model, applicator, results]
+    // retrieve selectors+ids from rdfstore and
+    // create annotation markers in document
+    // add triples to markers
+
+    return [model, applicator, annotator]
 }
 
+// TODO: define clean interface for plugin, Annotator & Applicator
+// TODO: RIP marginotes
+
 export default {
-    initialize: initialize
+    initialize: initialize,
+    applicator: applicator,
+    annotator: annotator,
+    tqa: TextQuoteAnchor
 }
