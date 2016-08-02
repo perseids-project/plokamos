415d01a1d0d963b7e57c67b213949f458af34644
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-08-02 17:05:32 -0400

    Merged with marginotes

--- src/lib/core/applicator.js
+++ src/lib/core/applicator.js
@@ -1,4 +1,5 @@
 import $ from 'jquery'
+import _ from 'lodash'
 import TextQuoteAnchor from 'dom-anchor-text-quote'
 
 // I have a list of selector types
@@ -65,6 +66,73 @@ class Applicator {
             ].join("\n")
         };
 
+        this.tooltip = (jqElement) => {
+            jqElement.hover(function (e) {
+                function stringify(obj) {
+                    return _.values(_.mapValues(obj,function(v,k) {
+                        var bonds = v
+                            .filter(function(o){
+                                return o.p === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && o.s.startsWith(k)
+                            })
+                            .map(function(o) {
+                                return o.s
+                            })
+                        var expressions = bonds.map(function(bond) {
+                            var subject = v.filter(function(o) {
+                                return o.p.endsWith("has-bond") && o.o === bond
+                            }).map(function(o) {return o.s})[0]
+                            var predicate = v.filter(function(o) {
+                                return o.p === "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" && o.s === bond
+                            }).map(function(o) {return o.o})[0]
+                            var object = v.filter(function(o) {
+                                return o.p.endsWith("bond-with") && o.s === bond
+                            }).map(function(o) {return o.o})[0]
+                            return subject.split("\/").slice(-1)[0]+"\n"+predicate+"\n"+object.split("\/").slice(-1)[0]
+                        })
+                        return expressions.join(";\n")
+                    })).join("\n\n")
+                }
+                var description = stringify($(this).data())//attr(field)
+                var tooltip = $('.margintooltip')
+
+                var menuState = document.documentElement.clientWidth - parseInt($("#menu-container").css('width'))
+                var deltaH = menuState ? 0 : parseInt($("#menu-container").css('height'));
+                var deltaW = menuState ? parseInt($("#menu-container").css('width')) : 0;
+
+                var parent = $(this.parentElement)
+                var position = parent.position()
+                var width = Math.min(100, position.left)
+
+                if (width < 60 || !description) {
+                    return
+                }
+
+                tooltip
+                    .css({
+                        'border-right': 'solid 2px #FF00FF',
+                        'font-size': '13px',
+                        'left': position.left - width - 5 + deltaW,
+                        'min-height': parent.height(),
+                        'padding-right': '7px',
+                        'position': 'absolute',
+                        'text-align': 'right',
+                        'top': position.top + deltaH,
+                        'width': width
+                    })
+                    .text(description)
+                    .stop()
+                    .fadeIn({
+                        duration:100,
+                        queue: false
+                    })
+            }, function () {
+                $('.margintooltip').stop()
+                $('.margintooltip').fadeOut({
+                    duration: 100
+                })
+            })
+        }
+
         /**
          * Load annotations and add markers to frontend
          * id is optional, loads all annotations if undefined
@@ -82,10 +150,9 @@ class Applicator {
                 .then((data) => _.last(data).result.forEach((x) => {
                     var element = $(document.getElementById(x.id.value))
                     var array = element.data(x.graph.value) || []
-                    element.data(x.graph.value,_.concat(array,{subject:x.subject.value, predicate:x.predicate.value,object:x.object.value}))
+                    element.data(x.graph.value,_.concat(array,{s:x.subject.value, p:x.predicate.value,o:x.object.value}))
+                    this.tooltip(element);
                 }))
-                .then((data) => $.getScript('js/marginotes.js'))
-                .then((data) => $('.perseids-annotation').marginotes({field:'id'})) // map data or get perseids-annotation class ?
         };
 
         /**
@@ -102,8 +169,9 @@ class Applicator {
                 }
                 if (parent) parent.removeChild( p[ 0 ] );
             }
-        }
+        };
 
+        $('body').append('<div class="margintooltip" style="display: none;"></div>')
         this.load();
     }
 
@@ -119,7 +187,10 @@ class Applicator {
         this.unload();
         this.load();
     }
-    
+
+    tooltip(element) {
+        this.tooltip(element)
+    }
 }
 
 export default Applicator
