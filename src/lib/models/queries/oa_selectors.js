bb74203a3419bfbfbc1fc62459dc3110f66f4266
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-08-22 17:38:08 -0400

    More generic OA selector model in SPARQL queries

--- src/lib/models/queries/oa_selectors.js
+++ src/lib/models/queries/oa_selectors.js
@@ -4,17 +4,24 @@
  */
 
 var Selectors = {
-    "http://www.w3.org/ns/oa#TextQuoteSelector": (id) => [
-        "SELECT ?id ?prefix ?exact ?suffix",
+    "http://www.w3.org/ns/oa#hasSelector": (id) => [
+        "SELECT ?id ?g ?s ?p ?o",
         "WHERE {",
         "GRAPH ?g {",
         `${id || "?id"} <http://www.w3.org/ns/oa#hasTarget> ?target .`,
-        "?target <http://www.w3.org/ns/oa#hasSelector> ?selector .",
-        "?selector <http://www.w3.org/ns/oa#prefix> ?prefix .",
-        "?selector <http://www.w3.org/ns/oa#exact> ?exact .",
-        "?selector <http://www.w3.org/ns/oa#suffix> ?suffix .",
+        "?target <http://www.w3.org/ns/oa#hasSelector> ?s .",
+        "?s ?p ?o",
         "}}"
-    ].join("\n")
+    ].join("\n"),
+    "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": (selectorTriples) => _.find(selectorTriples,(t) => t.p.value.endsWith("type")).o.value,
+    "http://www.w3.org/ns/oa#TextQuoteSelector": (selectorTriples) => {
+        var selectorObject = {}
+        selectorObject.type = _.find(selectorTriples,(t) => t.p.value.endsWith("type")).o.value
+        selectorObject.prefix = _.find(selectorTriples,(t) => t.p.value.endsWith("prefix")).o.value
+        selectorObject.exact = _.find(selectorTriples,(t) => t.p.value.endsWith("exact")).o.value
+        selectorObject.suffix = _.find(selectorTriples,(t) => t.p.value.endsWith("suffix")).o.value
+        return selectorObject
+    }
 }
 
 export default Selectors
