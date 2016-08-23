cee73cf58e6af7357486fb23235b00828943a95e
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-08-23 16:54:01 -0400

    Switch Applicator to generic OA selector model

--- src/lib/controllers/applicator.js
+++ src/lib/controllers/applicator.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 import _ from 'lodash'
 import Selectors from '../models/queries/oa_selectors'
-import Graph from '../models/queries/oa_bodies'
+import Annotation from '../models/queries/oabyId'
 import TextQuoteAnchor from 'dom-anchor-text-quote'
 import wrapRangeText from 'wrap-range-text'
 import NodeLink from '../views/applicator/NodeLink'
@@ -9,6 +9,9 @@ import Tooltip from '../views/applicator/Tooltip'
 import Delete from '../views/applicator/Delete'
 import SNAP from '../models/ontologies/SNAP'
 
+class Marker {
+
+}
 // I have a list of selector types
 // I have a list of queries to get selector data
 // I have a list of functions to apply
@@ -17,8 +20,15 @@ import SNAP from '../models/ontologies/SNAP'
  *
  */
 class Applicator {
+
     
     constructor (model) {
+
+        // todo: hash selector in Id class
+        var Id = {
+            fromSelector: (selector) => ((selector.prefix||"")+selector.exact+(selector.suffix||""))
+        }
+
         this.model = model;
         this.escape = (s) => s.replace(/[-/\\^$*+?.()（）|[\]{}]/g, '\\$&').replace(/\$/g, '$$$$');
 
@@ -27,23 +37,15 @@ class Applicator {
          * @type {{[http://www.w3.org/ns/oa#TextQuoteSelector]: ((p1:*, p2:*))}}
          */
         this.mark = {
-            "http://www.w3.org/ns/oa#TextQuoteSelector": (sel) => {
-                var selector = {}
-                if(sel.prefix) selector.prefix = sel.prefix.value
-                if (sel.exact) selector.exact = sel.exact.value
-                if (sel.suffix) selector.suffix = sel.suffix.value
-
+            "http://www.w3.org/ns/oa#TextQuoteSelector": (selector) => {
                 var span = document.createElement('span')
-                span.setAttribute("id",sel.id.value)
+                span.setAttribute("id",Id.fromSelector(selector))
                 span.classList.add("perseids-annotation")
-                span.setAttribute('data-prefix',selector.prefix)
-                span.setAttribute('data-exact',selector.exact)
-                span.setAttribute('data-suffix',selector.suffix)
-
+                span.setAttribute("data-selector",JSON.stringify(selector))
                 var textquote = TextQuoteAnchor.fromSelector(document.getElementById("annotator-main"),selector)
                 var range = textquote.toRange()
-
                 wrapRangeText(span,range)
+                return span;
             }
         };
 
@@ -53,29 +55,47 @@ class Applicator {
          * @private
          * @param id (optional) annotation id to query
          */
-        this.load = (id) => {
-            // get TextSelectors
-            this.model.execute(Selectors["http://www.w3.org/ns/oa#TextQuoteSelector"](id))
-            // mark positions in HTML
-                .then((selectors) => _.last(selectors).result.map((x) => this.mark["http://www.w3.org/ns/oa#TextQuoteSelector"](x)))
-                // get triples
-                .then((data) => this.model.execute(Graph["http://www.w3.org/ns/oa#hasBody"](id)))
+        this.load = (id) =>
+            this.model.execute(Annotation.byIdentifier(id))
+            .then((bindings) =>
+                _.groupBy(_.last(bindings).result,'id.value')
+            ).then((grouped) => {
+                var store = {}
+                    var spans = _.map(grouped, (v, k) => {
+                        var selectorURI = _.find(v, (obj) => obj.p.value.endsWith("hasSelector")).o.value
+                        var selectorTriples = v.filter((obj) => obj.s.value === selectorURI)
+                        var selectorType = Selectors["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"](selectorTriples)
+                        var selectorObject = Selectors[selectorType](selectorTriples)
+                        var idFromSelector = Id.fromSelector(selectorObject)
+                        var span = document.getElementById(idFromSelector) || this.mark[selectorType](selectorObject)
+                        if (!store[idFromSelector]) {store[idFromSelector] = {}}
+                        store[idFromSelector][k] = v
+                        return span
+                    })
 
-                .then((data) => _.last(data).result.map((x) => {
-                    var element = $(document.getElementById(x.id.value))
-                    var array = element.data(x.graph.value) || []
-                    element.data(x.graph.value,_.concat(array,{s:x.subject.value, p:x.predicate.value,o:x.object.value}))
+                return _.uniq(spans).map((span) => {
+                    var data = store[span.getAttribute('id')]
+                    var element = document.getElementById(span.getAttribute('id'))
+                    element.setAttribute('data-annotations',JSON.stringify(data))
+                    return $(element)
+                })
+                }
+            ) // todo: describe next step
+            .then((elements) =>
+                elements.map((element) => {
                     this.tooltip.register(element);
                     this.delete.register(element);
-                    return {g: x.graph.value, s:x.subject.value, p:x.predicate.value,o:x.object.value}
-                }))
-                // then add global view
-                .then((data) => {
-                    var snap = SNAP.simplify(_.groupBy(data,'g'))
+                    return element
+                })
+            ) // todo: describe next step
+            // todo: group data by id
+            .then((elements) => {
+                    var grouped = elements.reduce((object, element) => _.merge(object,element.data('annotations')), {})
+                    var snap = SNAP.simplify(grouped)
                     var input = _.flatMap(snap,(v,k)=>v.map((o) => Object.assign(o,{g:k})))
                     this.nodelink.add(input)
-                })
-        };
+                }
+            )
 
         /**
          * Remove annotation markers from frontend
