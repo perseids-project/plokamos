e4fb13cf2c493b8265cc60284d9012459535af91
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-08-29 18:41:10 -0400

    Update SPARQL model

--- src/lib/models/sparql.js
+++ src/lib/models/sparql.js
@@ -1,33 +1,30 @@
 import _ from 'lodash'
 
-var bindings2insert = (bindings) => {
-    // check head length, decide whether everything goes into default or split for named graphs
-    // if named graphs,
-    // groupBy named graphs
-    var grouped = _.groupBy(bindings, (triple) => triple.g.value);
-    // mapped bindings to triples
-    var mapped = _.mapValues(grouped, (graph) => graph.map((triple) => {
-            _.unset(triple, 'g');
-            var simple_triple = _.mapValues(triple, (element) => {
-                switch (element.type) {
-                    case "uri":
-                        return "<" + element.value + ">";
-                    case "bnode":
-                        return "<_:" + element.value + ">";
-                    case "literal":
-                        return element.datatype ? "\"" + element.value + "\"^^<" + element.datatype + ">" : "\"" + element.value + "\"";
-                }
-            });
-            return simple_triple.s + " " + simple_triple.p + " " + simple_triple.o + " .";
-        })
-    );
-    // return SPARQL INSERT queries
-    return _.map(_.keys(mapped), (k) => {
-        var graph = k.indexOf(":") + 1 ? k : "_:" + k
-        return "INSERT DATA { GRAPH <" + graph + "> {\n" + mapped[k].join("\n") + "\n}}";
-    });
+/**
+ * Mostly utility functions to deal with SPARQL formats and operations
+ * There are currently 3 formats in use:
+ * GSPO is a lightweight quad object without data typing,
+ * bindings are JSON serializations of SPARQL responses -> always keep GSPO notation!
+ * SPARQL is string serialization for SPARQL STATEMENTS -> use GRAPH keyword!
+ */
+
+class SPARQL {
+    static gspoToBinding(gspo) {return _.mapValues(gspo, (prop) => ""/* todo: switch for uri, literal, bnode with a regex (what about hypothesis?)*/)}
+    static bindingToGspo(binding) {return _.mapValues(binding,(prop) => prop.value)}
+    static bindingToSPARQL(binding) {
+        var strings = _.mapValues(binding, (prop) => { switch (prop.type) {
+            case "uri": return `<${prop.value}>`
+            case "literal": return !prop.datatype ? `"prop.value"` : `"${prop.value}"^^<${prop.datatype}>`
+            case "bnode": return prop.value.startsWith("_:") ? prop.value : `_:${prop.value}`
+        }})
+        return strings.g ? `GRAPH ${strings.g} {${strings.s} ${strings.p} ${strings.o} }` : `${strings.s} ${strings.p} ${strings.o} .`
+    }
+    static bindingsToDelete(bindings) {
+        return _.flatten(["DELETE DATA {",bindings.map(this.bindingToSPARQL),"}"]).join("\n")
+    }
+    static bindingsToInsert(bindings) {
+        return _.flatten(["INSERT DATA {",bindings.map(this.bindingToSPARQL),"}"]).join("\n")
+    }
 }
 
-export default {
-    bindings2insert: bindings2insert
-}
+export default SPARQL
