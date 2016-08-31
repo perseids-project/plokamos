dbb89716f3107031d9f2c525cc0b85fece5ae886
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-08-31 15:37:55 -0400

    Remove redundant code

--- src/lib/models/queries/oa_selectors.js
+++ src/lib/models/queries/oa_selectors.js
@@ -4,6 +4,7 @@
  */
 
 var Selectors = {
+    // todo: figure out if still used anywhere; move to ontologies/OA.js; i need a system to select queries depending on arguments
     "http://www.w3.org/ns/oa#hasSelector": (id) => [
         "SELECT ?id ?g ?s ?p ?o",
         "WHERE {",
@@ -12,16 +13,6 @@ var Selectors = {
         "?target <http://www.w3.org/ns/oa#hasSelector> ?s .",
         "?s ?p ?o",
         "}}"
-    ].join("\n"),
-    "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": (selectorTriples) => _.find(selectorTriples,(t) => t.p.value.endsWith("type")).o.value,
-    "http://www.w3.org/ns/oa#TextQuoteSelector": (selectorTriples) => {
-        var selectorObject = {}
-        selectorObject.type = _.find(selectorTriples,(t) => t.p.value.endsWith("type")).o.value
-        selectorObject.prefix = _.find(selectorTriples,(t) => t.p.value.endsWith("prefix")).o.value
-        selectorObject.exact = _.find(selectorTriples,(t) => t.p.value.endsWith("exact")).o.value
-        selectorObject.suffix = _.find(selectorTriples,(t) => t.p.value.endsWith("suffix")).o.value
-        return selectorObject
-    }
-}
+    ].join("\n")}
 
 export default Selectors
