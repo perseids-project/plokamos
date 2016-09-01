e3a9929c0ee64914869fd43128a830b240d56af0
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-08-31 20:32:52 -0400

    Variables can't be static

--- src/lib/models/ontologies/SNAP.js
+++ src/lib/models/ontologies/SNAP.js
@@ -1,68 +1,18 @@
 import Utils from '../../utils'
 
-class SNAP {
-    static labels = {
-        "http://data.snapdrgn.net/ontology/snap#AcknowledgedFamilyRelationship":"Has Acknowledged Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#AdoptedFamilyRelationship":"Has Adopted Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#AllianceWith":"Has Alliance With",
-        "http://data.snapdrgn.net/ontology/snap#AncestorOf":"Is Ancestor Of",
-        "http://data.snapdrgn.net/ontology/snap#AuntOf":"Is Aunt Of",
-        "http://data.snapdrgn.net/ontology/snap#Bond":"Has Bond With",
-        "http://data.snapdrgn.net/ontology/snap#BrotherOf":"Is Brother Of",
-        "http://data.snapdrgn.net/ontology/snap#CasualIntimateRelationshipWith":"Has Casual Intimate Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#ChildOf":"Is Child Of",
-        "http://data.snapdrgn.net/ontology/snap#ChildOfSiblingOf":"Is ChildOfSibling Of",
-        "http://data.snapdrgn.net/ontology/snap#ClaimedFamilyRelationship":"Has Claimed Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#CousinOf":"Is Cousin Of",
-        "http://data.snapdrgn.net/ontology/snap#DaughterOf":"Is Daughter Of",
-        "http://data.snapdrgn.net/ontology/snap#DescendentOf":"Is Descendent Of",
-        "http://data.snapdrgn.net/ontology/snap#EmnityFor":"Has Emnity For",
-        "http://data.snapdrgn.net/ontology/snap#ExtendedFamilyOf":"Is Extended Family Of",
-        "http://data.snapdrgn.net/ontology/snap#ExtendedHouseholdOf":"Is Extended Household Of",
-        "http://data.snapdrgn.net/ontology/snap#FamilyOf":"Is Family Of",
-        "http://data.snapdrgn.net/ontology/snap#FatherOf":"Is Father Of",
-        "http://data.snapdrgn.net/ontology/snap#FosterFamilyRelationship":"Has Foster Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#FreedSlaveOf":"Is Freed Slave Of",
-        "http://data.snapdrgn.net/ontology/snap#FreedmanOf":"Is Freedman Of",
-        "http://data.snapdrgn.net/ontology/snap#FreedwomanOf":"Is Freedwoman Of",
-        "http://data.snapdrgn.net/ontology/snap#FriendshipFor":"Has Friendship For",
-        "http://data.snapdrgn.net/ontology/snap#GrandchildOf":"Is Grandchild Of",
-        "http://data.snapdrgn.net/ontology/snap#GranddaughterOf":"Is Granddaughter Of",
-        "http://data.snapdrgn.net/ontology/snap#GrandfatherOf":"Is Grandfather Of",
-        "http://data.snapdrgn.net/ontology/snap#GrandmotherOf":"Is Grandmother Of",
-        "http://data.snapdrgn.net/ontology/snap#GrandparentOf":"Is Grandparent Of",
-        "http://data.snapdrgn.net/ontology/snap#GrandsonOf":"Is Grandson Of",
-        "http://data.snapdrgn.net/ontology/snap#GreatGrandfatherOf":"Is GreatGrandfather Of",
-        "http://data.snapdrgn.net/ontology/snap#GreatGrandmotherOf":"Is GreatGrandmother Of",
-        "http://data.snapdrgn.net/ontology/snap#GreatGrandparentOf":"Is GreatGrandparent Of",
-        "http://data.snapdrgn.net/ontology/snap#HalfFamilyRelationship":"HalfFamilyRelationship",
-        "http://data.snapdrgn.net/ontology/snap#HereditaryFamilyOf":"Is HereditaryFamily Of",
-        "http://data.snapdrgn.net/ontology/snap#HouseSlaveOf":"Is HouseSlave Of",
-        "http://data.snapdrgn.net/ontology/snap#HouseholdOf":"Is Household Of",
-        "http://data.snapdrgn.net/ontology/snap#InLawFamilyRelationship":"Has In-Law Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#IntimateRelationshipWith":"Has Intimate Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#KinOf":"Is Kin Of",
-        "http://data.snapdrgn.net/ontology/snap#LegallyRecognisedRelationshipWith":"Has Legally Recognised Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#Link":"Has Link With",
-        "http://data.snapdrgn.net/ontology/snap#MaternalFamilyRelationship":"Has Maternal Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#MotherOf":"Is Mother Of",
-        "http://data.snapdrgn.net/ontology/snap#NephewOf":"Is Nephew Of",
-        "http://data.snapdrgn.net/ontology/snap#NieceOf":"Is Niece Of",
-        "http://data.snapdrgn.net/ontology/snap#ParentOf":"Is Parent Of",
-        "http://data.snapdrgn.net/ontology/snap#PaternalFamilyRelationship":"Has Paternal Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#ProfessionalRelationship":"Has Professional Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#QualifierRelationship":"Has Qualifier Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#SeriousIntimateRelationshipWith":"Has Serious Intimate Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#SiblingOf":"Is Sibling Of",
-        "http://data.snapdrgn.net/ontology/snap#SiblingOfParentOf":"Is SiblingOfParent Of",
-        "http://data.snapdrgn.net/ontology/snap#SisterOf":"Is Sister Of",
-        "http://data.snapdrgn.net/ontology/snap#SlaveOf":"Is Slave Of",
-        "http://data.snapdrgn.net/ontology/snap#SonOf":"Is Son Of",
-        "http://data.snapdrgn.net/ontology/snap#StepFamilyRelationship":"Has Step Family Relationship With",
-        "http://data.snapdrgn.net/ontology/snap#UncleOf":"Is Uncle Of"
-    }
 
-    static expandMap = {
+var namespaces = [
+        {
+            prefix: "snap:",
+            uri: "http://data.snapdrgn.net/ontology/snap#"
+        },
+        {
+            prefix:"perseusrdf:",
+            uri:"http://data.perseus.org/"
+        }
+    ]
+
+var expandMap = {
         "default": () =>
             (gspo, graphs) => {
                 var annotation = graphs[gspo.g]
@@ -80,7 +30,7 @@ class SNAP {
             }
     }
 
-    static simplifyMap = {
+var simplifyMap = {
         "default": (obj) => {
             return _.mapValues(obj, function (v, k) {
                 var bonds = v
@@ -99,8 +49,97 @@ class SNAP {
         }
     }
 
-    static expand(type) { return _.result(expandMap,type,expandMap.default(type)) }
-    static simplify(type) { return _.result(simplifyMap,type,simplifyMap.default(type)) }
+class SNAP {
+    static labels(resource) {
+        var map = {
+            "http://data.snapdrgn.net/ontology/snap#AcknowledgedFamilyRelationship": "Has Acknowledged Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#AdoptedFamilyRelationship": "Has Adopted Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#AllianceWith": "Has Alliance With",
+            "http://data.snapdrgn.net/ontology/snap#AncestorOf": "Is Ancestor Of",
+            "http://data.snapdrgn.net/ontology/snap#AuntOf": "Is Aunt Of",
+            "http://data.snapdrgn.net/ontology/snap#Bond": "Has Bond With",
+            "http://data.snapdrgn.net/ontology/snap#BrotherOf": "Is Brother Of",
+            "http://data.snapdrgn.net/ontology/snap#CasualIntimateRelationshipWith": "Has Casual Intimate Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#ChildOf": "Is Child Of",
+            "http://data.snapdrgn.net/ontology/snap#ChildOfSiblingOf": "Is ChildOfSibling Of",
+            "http://data.snapdrgn.net/ontology/snap#ClaimedFamilyRelationship": "Has Claimed Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#CousinOf": "Is Cousin Of",
+            "http://data.snapdrgn.net/ontology/snap#DaughterOf": "Is Daughter Of",
+            "http://data.snapdrgn.net/ontology/snap#DescendentOf": "Is Descendent Of",
+            "http://data.snapdrgn.net/ontology/snap#EmnityFor": "Has Emnity For",
+            "http://data.snapdrgn.net/ontology/snap#ExtendedFamilyOf": "Is Extended Family Of",
+            "http://data.snapdrgn.net/ontology/snap#ExtendedHouseholdOf": "Is Extended Household Of",
+            "http://data.snapdrgn.net/ontology/snap#FamilyOf": "Is Family Of",
+            "http://data.snapdrgn.net/ontology/snap#FatherOf": "Is Father Of",
+            "http://data.snapdrgn.net/ontology/snap#FosterFamilyRelationship": "Has Foster Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#FreedSlaveOf": "Is Freed Slave Of",
+            "http://data.snapdrgn.net/ontology/snap#FreedmanOf": "Is Freedman Of",
+            "http://data.snapdrgn.net/ontology/snap#FreedwomanOf": "Is Freedwoman Of",
+            "http://data.snapdrgn.net/ontology/snap#FriendshipFor": "Has Friendship For",
+            "http://data.snapdrgn.net/ontology/snap#GrandchildOf": "Is Grandchild Of",
+            "http://data.snapdrgn.net/ontology/snap#GranddaughterOf": "Is Granddaughter Of",
+            "http://data.snapdrgn.net/ontology/snap#GrandfatherOf": "Is Grandfather Of",
+            "http://data.snapdrgn.net/ontology/snap#GrandmotherOf": "Is Grandmother Of",
+            "http://data.snapdrgn.net/ontology/snap#GrandparentOf": "Is Grandparent Of",
+            "http://data.snapdrgn.net/ontology/snap#GrandsonOf": "Is Grandson Of",
+            "http://data.snapdrgn.net/ontology/snap#GreatGrandfatherOf": "Is GreatGrandfather Of",
+            "http://data.snapdrgn.net/ontology/snap#GreatGrandmotherOf": "Is GreatGrandmother Of",
+            "http://data.snapdrgn.net/ontology/snap#GreatGrandparentOf": "Is GreatGrandparent Of",
+            "http://data.snapdrgn.net/ontology/snap#HalfFamilyRelationship": "HalfFamilyRelationship",
+            "http://data.snapdrgn.net/ontology/snap#HereditaryFamilyOf": "Is HereditaryFamily Of",
+            "http://data.snapdrgn.net/ontology/snap#HouseSlaveOf": "Is HouseSlave Of",
+            "http://data.snapdrgn.net/ontology/snap#HouseholdOf": "Is Household Of",
+            "http://data.snapdrgn.net/ontology/snap#InLawFamilyRelationship": "Has In-Law Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#IntimateRelationshipWith": "Has Intimate Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#KinOf": "Is Kin Of",
+            "http://data.snapdrgn.net/ontology/snap#LegallyRecognisedRelationshipWith": "Has Legally Recognised Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#Link": "Has Link With",
+            "http://data.snapdrgn.net/ontology/snap#MaternalFamilyRelationship": "Has Maternal Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#MotherOf": "Is Mother Of",
+            "http://data.snapdrgn.net/ontology/snap#NephewOf": "Is Nephew Of",
+            "http://data.snapdrgn.net/ontology/snap#NieceOf": "Is Niece Of",
+            "http://data.snapdrgn.net/ontology/snap#ParentOf": "Is Parent Of",
+            "http://data.snapdrgn.net/ontology/snap#PaternalFamilyRelationship": "Has Paternal Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#ProfessionalRelationship": "Has Professional Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#QualifierRelationship": "Has Qualifier Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#SeriousIntimateRelationshipWith": "Has Serious Intimate Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#SiblingOf": "Is Sibling Of",
+            "http://data.snapdrgn.net/ontology/snap#SiblingOfParentOf": "Is SiblingOfParent Of",
+            "http://data.snapdrgn.net/ontology/snap#SisterOf": "Is Sister Of",
+            "http://data.snapdrgn.net/ontology/snap#SlaveOf": "Is Slave Of",
+            "http://data.snapdrgn.net/ontology/snap#SonOf": "Is Son Of",
+            "http://data.snapdrgn.net/ontology/snap#StepFamilyRelationship": "Has Step Family Relationship With",
+            "http://data.snapdrgn.net/ontology/snap#UncleOf": "Is Uncle Of"
+        }
+        return map[resource]
+    }
+
+    /*static expandMap(resource) {
+        var map = {
+            "default": () =>
+            (gspo, graphs) => {
+                var annotation = graphs[gspo.g]
+                var bond_id = annotation ? undefined : gspo.g + "-bond-" + Utils.hash(JSON.stringify(gspo)).slice(0, 4)
+                return annotation ? annotation.filter((triple) =>
+                    (quad.p.value.endsWith('has-bond') && quad.s.value === gspo.s)
+                    || (quad.p.value.endsWith('type') && quad.o.value === gspo.p)
+                    || (quad.p.value.endsWith('bond-with') && quad.o.value === gspo.o)
+                ) :
+                    [
+                        {g: gspo.g, s: bond_id, p: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", o: gspo.p},
+                        {g: gspo.g, s: gspo.s, p: "http://data.snapdrgn.net/ontology/snap#has-bond", o: bond_id},
+                        {g: gspo.g, s: bond_id, p: "http://data.snapdrgn.net/ontology/snap#bond-with", o: gspo.o},
+                    ]
+            }
+        }
+        return map[resource]
+    }*/
+
+
+
+    // TODO: test and move labels into var
+    static expand(type) { return _.get(expandMap,type,expandMap.default(type)) }
+    static simplify(type) { return _.get(simplifyMap,type,simplifyMap.default(type)) }
 
     static label (uri) {
         var term = uri.replace(namespaces.snap.uri,'').replace(namespaces.snap.prefix,'').replace(namespaces.perseus.uri,'').replace(namespaces.perseus.prefix,'')
@@ -170,16 +209,6 @@ class SNAP {
         }
         return labels[term] || uri
     }
-    static namespaces = [
-        {
-            prefix: "snap:",
-            uri: "http://data.snapdrgn.net/ontology/snap#"
-        },
-        {
-            prefix:"perseusrdf:",
-            uri:"http://data.perseus.org/"
-        }
-    ]
 }
 
 export default SNAP
