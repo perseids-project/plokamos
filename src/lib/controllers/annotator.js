785174fa6708aa496c5e673b983a1fe9a23c0e0f
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-01 10:03:23 -0400

    Implemented CREATE in Annotator

--- src/lib/controllers/annotator.js
+++ src/lib/controllers/annotator.js
@@ -14,7 +14,8 @@ class Annotator {
     // API: create(fragment), update(fragments), delete(fragment), drop(graph)
 
     constructor(app) {
-
+        // TODO: add controls for history, save at bottom of anchor
+        this.anchor = app.anchor
         this.model = app.model;
         this.applicator = app.applicator;
         this.history = app.history;
@@ -28,7 +29,7 @@ class Annotator {
             "cite": (pre,post) => "http://data.perseus.org/collections/urn:cite:perseus:pdljann."+Utils.hash(pre)+Utils.hash(post),
             "user": () => $('#annotator-main').data().user,
             "urn": () => $('#annotator-main').data().urn,
-            "date": () => (new Date()).toISOString(),
+            "date": () => ,
             "triple": () => {
                 return {
                     subject: ($("#subject_prefixes").data('url')|"")+$("#create_subject > span > input").last().val(),
@@ -116,154 +117,66 @@ class Annotator {
          *
          * @param list
          */
-        this.create = (list) => {}
-        // TODO: use acquire to build annotation, mae sure its bindings not GSPOs
-    }
-    
-    save () {
+        this.create = (annotationId, bindings) => {
+            var defaultGraph = "http://data.perseus.org/graphs/people"
+            // todo: figure out default graph for use cases (maybe motivatedBy, by plugin or manual in anchor?)
+            var selectorId = _.find(bindings,(binding) => binding.p.value === "http://www.w3.org/ns/oa#exact").s.value
+            // todo: make independent of selector type
+            var targetId = annotationId+"#target-"+Utils.hash(JSON.stringify(selectorId)).slice(0, 4)
+            var userId = app.anchor.data('user')
+            var urn = app.anchor.data('urn')
+            var oa =[
+                {"g": { "type":"uri", "value": defaultGraph },
+                    "s": { "type":"uri", "value": annotationId },
+                    "p": { "type":"uri", "value": "rdf:type" },
+                    "o": { "type":"uri", "value": "oa:Annotation" }},
+                {"g": { "type":"uri", "value": defaultGraph },
+                    "s": { "type":"uri", "value": annotationId },
+                    "p": { "type":"uri", "value": "dcterms:source" },
+                    "o": { "type":"uri", "value": "https://github.com/fbaumgardt/perseids-annotator" }},
+                {"g": { "type":"uri", "value": defaultGraph },
+                    "s": { "type":"uri", "value": annotationId },
+                    "p": { "type":"uri", "value": "oa:serializedBy" },
+                    "o": { "type":"uri", "value": "https://github.com/fbaumgardt/perseids-annotator" }}
+            ]
 
-        // acquire insert parameters
-        var selector = this.acquire.selector();
-        selector.urn = this.acquire.urn();
-        var date = this.acquire.date();
-        var triple = {s:this.acquire.triple().subject,p:this.acquire.triple().predicate,o:this.acquire.triple().object};
-        var user = this.acquire.user(); // agent -> nemo or oauth
-        var cite = this.acquire.cite(user+selector.urn,date+selector.prefix+selector.exact+selector.suffix+triple.s+triple.p+triple.o)
-        triple.g = cite
+            var target = [
+                {"p": { "type":"uri", "value":"oa:hasTarget" },
+                    "g": { "type":"uri", "value":defaultGraph },
+                    "s": { "type":"uri", "value":annotationId },
+                    "o": { "type":"uri", "value":targetId }},
+                {"p": { "type":"uri", "value":"rdf:type" },
+                    "g": { "type":"uri", "value":defaultGraph },
+                    "s": { "type":"uri", "value":targetId },
+                    "o": { "type":"uri", "value":"oa:SpecificResource" }}, // todo: figure out alternatives for non-text targets
+                {"p": { "type":"uri", "value":"oa:hasSource" },
+                    "g": { "type":"uri", "value":defaultGraph },
+                    "s": { "type":"uri", "value":targetId },
+                    "o": { "type":"uri", "value":urn}},
+                {"p": { "type":"uri", "value":"oa:hasSelector" },
+                    "g": { "type":"uri", "value":defaultGraph },
+                    "s": { "type":"uri", "value":targetId },
+                    "o": { "type":"uri", "value":selectorId }}
+            ]
 
-        // bindings2insert -> model.execute
+            var date = [{
+                "p": { "type":"uri", "value":"oa:annotatedAt" },
+                "g": { "type":"uri", "value": defaultGraph},
+                "s": { "type":"uri", "value":annotationId },
+                "o": { "datatype": "http://www.w3.org/2001/XMLSchema#dateTimeStamp", "type":"literal", "value": (new Date()).toISOString()}
+            }]
 
-        // annotation -> get from input fields
-        var binds = ((cite, graph) => { return {
-            "oa":() => [
-            {"g": { "type":"uri", "value": graph || "http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value": cite },
-                "p": { "type":"uri", "value": "rdf:type" },
-                "o": { "type":"uri", "value": "oa:Annotation" }},
-            {"g": { "type":"uri", "value": graph || "http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value": cite },
-                "p": { "type":"uri", "value": "dcterms:source" },
-                "o": { "type":"uri", "value": "https://hypothes.is/api/annotations/oM3uKsoSRXmPfg1vsNSfxw" }},
-            {"g": { "type":"uri", "value": graph || "http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value": cite },
-                "p": { "type":"uri", "value": "oa:serializedBy" },
-                "o": { "type":"uri", "value": "https://github.com/fbaumgardt/perseids-annotator" }}],
-            "date": (date) => [
-            {"p": { "type":"uri", "value":"oa:annotatedAt" },
-                "g": { "type":"uri", "value": graph || "http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite },
-                "o": { "datatype": "http://www.w3.org/2001/XMLSchema#dateTimeStamp", "type":"literal", "value":date }}
-        ],
-            "user": (group) => [
-            {"p": { "type":"uri", "value":"oa:annotatedBy" },
-                "g": { "type":"uri", "value":graph || "http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value": cite },
-                "o": { "type":"uri", "value":group }} // NOTE: describe <o> query
-        ],
-            "target": (target) => [
-            {"p": { "type":"uri", "value":"oa:hasTarget" },
-                "g": { "type":"uri", "value":graph || "http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite },
-                "o": { "type":"uri", "value":cite+"#target-1" }},
-            {"p": { "type":"uri", "value":"rdf:type" },
-                "g": { "type":"uri", "value":graph || "http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite+"#target-1" },
-                "o": { "type":"uri", "value":"oa:SpecificResource" }},
-            {"p": { "type":"uri", "value":"oa:hasSource" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite+"#target-1" },
-                "o": { "type":"uri", "value":target.urn }},
-            {"p": { "type":"uri", "value":"oa:hasSelector" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite+"#target-1" },
-                "o": { "type":"uri", "value":cite+"#target-1-sel-1" }},
-            {"p": { "type":"uri", "value":"rdf:type" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite+"#target-1-sel-1" },
-                "o": { "type":"uri", "value":"oa:TextQuoteSelector" }},
-            {"p": { "type":"uri", "value":"oa:prefix" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite+"#target-1-sel-1" },
-                "o": { "type":"literal", "value":target.prefix }},
-            {"p": { "type":"uri", "value":"oa:exact" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite+"#target-1-sel-1" },
-                "o": { "type":"literal", "value":target.exact }},
-            {"p": { "type":"uri", "value":"oa:suffix" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite+"#target-1-sel-1" },
-                "o": { "type":"literal", "value":target.suffix }}
-        ],
-            "annotation": (triple) => [ // note: SNAP ONTOLOGY + how to do title??
-            {"p": { "type":"uri", "value":"dcterms:title" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite },
-                "o": { "type":"literal", "value":"http://data.perseus.org/people/smith:tecmessa-1#this identifies Tecmessa as snap:IntimateRelationship in urn:cts:pdlrefwk:viaf88890045.003.perseus-eng1:A.ajax_1" }},
-            {"p": { "type":"uri", "value":"oa:motivatedBy" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite },
-                "o": { "type":"uri", "value":"oa:identifying" }},
-            {"p": { "type":"uri", "value":"oa:hasBody" },
-                "g": { "type":"uri", "value":graph||"http://data.perseus.org/graphs/people" },
-                "s": { "type":"uri", "value":cite },
-                "o": { "type":"uri", "value":cite }},
-            {"p": { "type":"uri", "value":"http://www.w3.org/1999/02/22-rdf-syntax-ns#type" },
-                "g": { "type":"uri", "value":cite },
-                "s": { "type":"uri", "value":cite+"#bond-1" },
-                "o": { "type":"uri", "value":triple.p }},
-            {"p": { "type":"uri", "value":"snap:has-bond" },
-                "g": { "type":"uri", "value":cite },
-                "s": { "type":"uri", "value":triple.s },
-                "o": { "type":"uri", "value":cite+"#bond-1" }},
-            {"p": { "type":"uri", "value":"snap:bond-with" },
-                "g": { "type":"uri", "value":cite },
-                "s": { "type":"uri", "value":cite+"#bond-1" },
-                "o": { "type":"uri", "value":triple.o }}
-        ]
-        }})(cite);
 
-        var bindings = {head:{vars:["s","p","o","g"]},results:{bindings:_.concat(
-            binds.oa(cite),
-            binds.date(date),
-            binds.user(user),
-            binds.target(selector),
-            binds.annotation(triple)
-        )}}
-
-        var insert = SPARQL.bindingsToInsert(bindings.results.bindings);
-
-        insert.forEach((sparql) => this.model.execute(sparql).then(
-            (r) => {
-
-                // clear inputs
-
-                $('#create_subject > span > input').last().val('');
-                $('#create_predicate > span > input').last().val('');
-                $('#create_object > span > input').last().val('');
-
-                // remove modal
-                $('#create_modal').toggle();
-                $('body').removeClass('modal-open');
-                $('.modal-backdrop').remove();
-                $('#create_modal').removeClass('in');
-
-                // add span
-                var span = document.createElement('span');
-                span.setAttribute('class','perseids-annotation');
-                span.setAttribute('id',cite);
-                this.currentRange.surroundContents(span);
-                var annotation_body = [
-                    {s:cite+"#bond-1",p:"http://www.w3.org/1999/02/22-rdf-syntax-ns#type",o:triple.p},
-                    {s:triple.s,p:"snap:has-bond",o:cite+"#bond-1"},
-                    {s:cite+"#bond-1",p:"snap:bond-with",o:triple.o}
+            var user = [
+                {"p": { "type":"uri", "value":"oa:annotatedBy" },
+                "g": { "type":"uri", "value": defaultGraph },
+                "s": { "type":"uri", "value": annotationId },
+                "o": { "type":"uri", "value": userId }} // NOTE: describe <o> query
                 ]
-                $(span).data(cite,annotation_body)
-                // applicator.load(id)
-                this.applicator.tooltip($(span))
-                // add command to history
 
-            }
-        ));
+            this.model.execute(SPARQL.bindingsToInsert(_.flatten(oa,date,user,target,bindings)))
+
+        }
     }
 }
 
