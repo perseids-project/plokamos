a76c9a29076d4a913e03aa571b9092f8b662e6dd
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-01 19:05:15 -0400

    SPARQL fully functional

--- src/lib/models/sparql.js
+++ src/lib/models/sparql.js
@@ -9,23 +9,44 @@ import _ from 'lodash'
  */
 
 class SPARQL {
-    static gspoToBinding(gspo) {return _.mapValues(gspo, (prop) => ""/* todo: switch for uri, literal, bnode with a regex (what about hypothesis?)*/)}
+    static gspoToBinding(gspo) {return _.mapValues(gspo, (prop) => {
+        if (prop.startsWith("http://") || prop.startsWith("http://")) {
+            return {type:"uri",value:prop}
+        } else if (prop.startsWith("_:")) {
+            return {type:"bnode",value:prop}
+        } else { // todo: come back and use regex properly
+            if (!prop.replace(/\d+/g,'')) {
+                return {type:"literal",datatype:"http://www.w3.org/2001/XMLSchema#int",value:prop}
+            } else if (prop.replace(/\d+/g,'').startsWith("--T::.")) {
+                return {type:"literal",datatype:"http://www.w3.org/2001/XMLSchema#dateTimeStamp",value:prop}
+            } else {
+                return {type:"literal",value:prop}
+            }
+        }
+    }/* todo: switch for uri, literal, bnode with a regex (what about hypothesis?)*/)
+    }
     static bindingToGspo(binding) {return _.mapValues(binding,(prop) => prop.value)}
     static bindingToSPARQL(binding) {
-        var strings = _.mapValues(binding, (prop) => { switch (prop.type) {
+        var strings = _.mapValues(binding, (prop) => { switch (prop.token || prop.type) {
             case "uri": return `<${prop.value}>`
-            case "literal": return !prop.datatype ? `"${prop.value}"` : `"${prop.value}"^^<${prop.datatype}>`
+            case "literal": return !(prop.datatype || (prop.token && prop.type)) ? `"${prop.value}"` : `"${prop.value}"^^<${prop.datatype || prop.type}>`
             case "bnode": return prop.value.startsWith("_:") ? prop.value : `_:${prop.value}`
         }})
         return strings.g ? `GRAPH ${strings.g} {${strings.s} ${strings.p} ${strings.o} }` : `${strings.s} ${strings.p} ${strings.o} .`
     }
     static bindingsToDelete(bindings) {
-        return _.flatten(["DELETE DATA {",bindings.map(this.bindingToSPARQL),"}"]).join("\n")
+        return _.chain(bindings)
+            .flatten()
+            .groupBy('g.value')
+            .map((v,k) => _.concat("DELETE DATA { GRAPH <"+k+"> {",v.map((quad) => SPARQL.bindingToSPARQL(_.omit(quad,'g'))),"}}").join("\n"))
+            .value()
     }
+
     static bindingsToInsert(bindings) {
         return _.chain(bindings)
+            .flatten()
             .groupBy('g.value')
-            .map((v,k) => _.concat("INSERT DATA { GRAPH <"+k+"> {",v.map((quad) => this.bindingToSPARQL(_.omit(quad,'g'))),"}}").join("\n"))
+            .map((v,k) => _.concat("INSERT DATA { GRAPH <"+k+"> {",v.map((quad) => SPARQL.bindingToSPARQL(_.omit(quad,'g'))),"}}").join("\n"))
             .value()
     }
 }
