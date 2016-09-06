f2b839910ff5ca665b59e67fd0d679d2ca2849e2
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-06 17:19:23 -0400

    Add History.commit

--- src/lib/models/history.js
+++ src/lib/models/history.js
@@ -2,6 +2,7 @@ import $ from 'jquery'
 
 class History {
     constructor(app) {
+        this.app = app
         this.model = app.model;
         this.applicator = app.applicator;
         this.commands = [];
@@ -10,7 +11,8 @@ class History {
 
     undo() {
         this.index -= 1
-        return this.commands.slice(0,this.index)
+        this.model.reset()
+        return this.model.execute(_.flatten(this.commands.slice(0,this.index)))
     }
 
     redo() {
@@ -30,6 +32,42 @@ class History {
         this.commands = [];
         this.index = 0
         return this.model.reset().then(() => this.applicator.reset())
+    }
+
+    commit(source) {
+        var endpoint = this.app.getEndpoint().write
+        var mime = "application/sparql-update"
+        var commands = _.chain(this.commands.slice(0,this.index)).flatten().value()
+        var sequence = commands.slice().reverse()
+
+        // TODO: possibly aggregate the responses
+        // TODO: do DROP last, because it cannot be reverted (easily)
+        var response = _.reduce(commands,(previous,current) =>
+            previous.then(
+                (success) => {
+                    if (success) sequence.pop() // we keep a register of outstanding operations
+                    if (source && source.progress) source.progress(1-sequence.length/commands.length)
+                    return $.ajax( { url:endpoint, type:'POST', data:current, contentType:mime} )
+                },
+                (failure) => $.Deferred().reject(sequence).promise()
+            ),
+            $.Deferred().resolve().promise()
+        )
+        response.then(
+            (success) => {
+                this.model.upstream.push(this.commands.slice(0,this.index))
+                this.reset()
+            },
+            (failure) => {
+                var successful = commands.slice(0,-1*sequence.length)
+                var failed = sequence.slice().reverse()
+                // todo: recover structure of commands
+                this.model.upstream.push(successful)
+                this.commands = [failed]
+                this.reset()
+                }
+        )
+        return response
     }
 }
 export default History
