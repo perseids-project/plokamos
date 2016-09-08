1c64d1e96bee4afd9c30b9f88a68bd078be3b18e
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-08 11:10:05 -0400

    Fixed dimensions

--- src/lib/views/applicator/NodeLink.js
+++ src/lib/views/applicator/NodeLink.js
@@ -7,9 +7,10 @@ class NodeLink {
         var body = app.anchor
         var self = this
         var globalViewBtn = $('<div class="btn btn-circle" id="global-view-btn" style="position: fixed; top:15%; right:5%; z-index:1000; background-color:black; color:white;"><span class="glyphicon glyphicon-link"></span></span></div>')
-        var globalView = $('<div class="well" id="global-view" style="position:fixed; top:10%; left:12.5%; width:75%; height:40%; z-index:1000; display:none;"/>');
+        var globalView = $('<div class="well" id="global-view" style="position:fixed; top:10%; left:12.5%; width:75%; height:40%; z-index:1000;"/>');
         body.append(globalViewBtn);
         body.append(globalView);
+        globalView.css('display','none');
         globalViewBtn.mouseleave(function(e) {if (!globalViewBtn.keep)$('#global-view').css('display','none')});
         globalViewBtn.mouseenter(function(e) {$('#global-view').css('display','block')});
         globalViewBtn.click(function(e) {
@@ -17,8 +18,6 @@ class NodeLink {
             $('#global-view').css('display','block')})
         globalViewBtn.keep = false;
         this.parent = globalView.get(0)
-        this.width = globalView.width()
-        this.height = globalView.height()
 
         this.USE_GRID = true;
         this.GRID_SIZE = 60;
@@ -29,18 +28,18 @@ class NodeLink {
         this.nodes = []
         this.links = []
 
-        this.vis = d3.select(this.parent).append("svg:svg").attr("width",this.width).attr("height",this.height)
-        this.force = d3.layout.force().size([this.width, this.height]).nodes(this.nodes).links(this.links).gravity(1).linkDistance(function(d){return (1-d.weight)*100}).charge(-3000).linkStrength(function(x) {
+        this.vis = d3.select(this.parent).append("svg:svg").attr("width","100%").attr("height","100%")
+        this.force = d3.layout.force().size([$(self.parent).width(), $(self.parent).height()]).nodes(this.nodes).links(this.links).gravity(1).linkDistance(function(d){return (1-d.weight)*100}).charge(-3000).linkStrength(function(x) {
             return x.weight * 5
         });
-        this.grid = function(width, height) {
+        this.grid = function() {
             return {
                 cells : [],
 
                 init : function() {
                     this.cells = [];
-                    for(var i = 0; i < width / self.GRID_SIZE; i++) {
-                        for(var j = 0; j < height / self.GRID_SIZE; j++) {
+                    for(var i = 0; i < $(self.parent).width() / self.GRID_SIZE; i++) {
+                        for(var j = 0; j < $(self.parent).height() / self.GRID_SIZE; j++) {
                             // HACK: ^should be a better way to determine number of rows and cols
                             var cell;
                             switch (self.GRID_TYPE) {
@@ -59,7 +58,7 @@ class NodeLink {
                                 case "HEXA":
                                     cell = {
                                         x : i * self.GRID_SIZE + (j % 2) * self.GRID_SIZE * .5,
-                                        y : j * self.GRID_SIZE * .85
+                                        y : j * self.GRID_SIZE
                                     };
                                     break;
                             }
@@ -87,7 +86,7 @@ class NodeLink {
                 }
 
             }
-        }(this.width, this.height);
+        }();
 
         this.updateLink = function() {
             this.attr("x1", (d) => {
@@ -122,6 +121,7 @@ class NodeLink {
             });
         };
         this.update_graph = () => {
+            self.force.size([$(self.parent).width(), $(self.parent).height()])
             self.link = self.vis.selectAll("line.link").data(
                 self.force.links(),
                 (d) => d.source.id + "-" + d.target.id
@@ -153,8 +153,8 @@ class NodeLink {
                 var subjectIdx = _.findIndex(self.nodes,['id',t.s])
                 var objectIdx = _.findIndex(self.nodes,['id',t.o])
                 var predicateIdx = (subjectIdx+1 && objectIdx+1) ? _.findIndex(self.links,{source:subjectIdx,target:objectIdx}) : -1
-                if (subjectIdx+1) {self.nodes[subjectIdx].graphs.push(t.g)} else {subjectIdx = self.nodes.push({id:t.s,graphs:[t.g],x:Math.floor(self.width*Math.random()),y:Math.floor(self.height*Math.random())})-1}
-                if (objectIdx+1) {self.nodes[objectIdx].graphs.push(t.g)} else {objectIdx = self.nodes.push({id:t.o,graphs:[t.g],x:Math.floor(self.width*Math.random()),y:Math.floor(self.height*Math.random())})-1}
+                if (subjectIdx+1) {self.nodes[subjectIdx].graphs.push(t.g)} else {subjectIdx = self.nodes.push({id:t.s,graphs:[t.g],x:Math.floor($(self.parent).width()*Math.random()),y:Math.floor($(self.parent).height()*Math.random())})-1}
+                if (objectIdx+1) {self.nodes[objectIdx].graphs.push(t.g)} else {objectIdx = self.nodes.push({id:t.o,graphs:[t.g],x:Math.floor($(self.parent).width()*Math.random()),y:Math.floor($(self.parent).height()*Math.random())})-1}
                 if (predicateIdx+1) {self.links[predicateIdx].graphs.push([t.g,t.p])} else {predicateIdx = self.links.push({source:subjectIdx,target:objectIdx,graphs:[[t.g,t.p]],weight:1})-1}
             })
             // todo
@@ -174,10 +174,12 @@ class NodeLink {
 
         this.update_graph()
         this.force.on("tick", () => {
+            self.force.size([$(self.parent).width(), $(self.parent).height()])
             self.vis.select("g.gridcanvas").remove();
             if(self.USE_GRID) {
                 self.grid.init();
-                var gridCanvas = self.vis.append("svg:g").attr("class", "gridcanvas").attr("width", self.width).attr("height", self.height);
+                var gridCanvas = self.vis.append("svg:g").attr("class", "gridcanvas").attr("width", "100%").attr("height", "100%");
+
                 _.each(self.grid.cells, (c) => gridCanvas.append("svg:circle").attr("cx", c.x).attr("cy", c.y).attr("r", 2).style("fill", "#555").style("opacity", .3));
             }
 
