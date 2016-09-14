7129006ea679344c0a89209ed6afe3bf47ef9033
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-13 20:31:05 -0400

    Switch Tooltip to Bootstrap Popover

--- src/lib/views/applicator/Tooltip.js
+++ src/lib/views/applicator/Tooltip.js
@@ -2,54 +2,21 @@ import SNAP from '../../models/ontologies/SNAP'
 
 class Tooltip {
     constructor(app) {
-        var jqParent = app.anchor
-        jqParent.append($('<div class="margintooltip" style="display: none;"></div>'))
         this.register = (jqElement) => {
-            jqElement.hover(function (e) {
-                // planned: stringify should check ontology and select simplifier or stringify raw (.value)
-                function stringify(obj) {
-                    var simplified = SNAP.simplify()(obj)
-                    return _.flatten(_.values(simplified)).map((o) => SNAP.label(o.s)+";\n"+SNAP.label(o.p)+";\n"+SNAP.label(o.o)).join("\n\n")
-                }
-                var graphs = $(this).data('annotations')
-                var description = stringify(graphs)//attr(field)
-                var tooltip = $('.margintooltip')
-
-                var menuState = document.documentElement.clientWidth - parseInt($("#menu-container").css('width'))
-                var deltaH = 0// menuState ? 0 : parseInt($("#menu-container").css('height'));
-                var deltaW = 0// menuState ? parseInt($("#menu-container").css('width')) : 0;
-                // planned: possibly factor out layout calculations into Utils or somewhere else?
-                var parent = $(this.parentElement)
-                var position = parent.position()
-                var width = Math.min(100, position.left)
-
-                if (width < 60 || !description) {
-                    return
-                }
-
-                tooltip
-                    .css({
-                        'border-right': 'solid 2px #FF00FF',
-                        'font-size': '13px',
-                        'left': position.left - width - 5 + deltaW,
-                        'min-height': parent.height(),
-                        'padding-right': '7px',
-                        'position': 'absolute',
-                        'text-align': 'right',
-                        'top': position.top + deltaH,
-                        'width': width
-                    })
-                    .text(description)
-                    .stop()
-                    .fadeIn({
-                        duration:100,
-                        queue: false
-                    })
-            }, function () {
-                $('.margintooltip').stop()
-                $('.margintooltip').fadeOut({
-                    duration: 100
-                })
+            // planned: stringify should check ontology and select simplifier or stringify raw (.value)
+            function stringify(obj) {
+                var simplified = SNAP.simplify()(obj)
+                return _.flatten(_.values(simplified)).map((o) => "<span class='tt-label tt-subject'>"+SNAP.label(o.s)+"</span><span class='tt-label tt-predicate'>"+SNAP.label(o.p)+"</span><span class='tt-label tt-object'>"+SNAP.label(o.o)+"</span>").join("<br>")
+            }
+            var graphs = jqElement.data('annotations')
+            var description = stringify(graphs)//attr(field)
+            jqElement.popover({
+                container:"body",
+                html:"true",
+                trigger: "hover | click",
+                placement: "auto top",
+                title: jqElement.data('selector').exact,
+                content: description
             })
         }
     }
