f023fc97de46ad864c73b532496d19f1e4179f3b
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-09-27 14:41:05 -0400

    Fail-safe History.commit (untested)

--- src/lib/models/history.js
+++ src/lib/models/history.js
@@ -58,11 +58,11 @@ class History {
             return this.commands
         }
 
-        this.reset = () => {
-            this.commands = [];
+        this.reset = (remaining) => {
+            this.commands = remaining || [];
             this.index = 0
             this.backBtn.prop('disabled',true)
-            this.forwardBtn.prop('disabled',true)
+            this.forwardBtn.prop('disabled',this.commands.length===0)
             this.commitBtn.prop('disabled',true)
             return this.model.reset().then(() => this.applicator.reset())
         }
@@ -70,9 +70,9 @@ class History {
         this.commit = (source) => {
             var endpoint = this.app.getEndpoint().write
             var mime = "application/sparql-update"
-            var commands = _.chain(this.commands.slice(0,this.index)).flatten().value()
+            var commands = _.chain(this.commands.slice(0,this.index)).map((v,k) => v.map((w) => {return {i:k,sparql:w}})).flatten().value()
             var sequence = commands.slice().reverse()
-
+            // todo: add parent-level index to commands before flattening, use to group in case of failure
             // todo: possibly aggregate the responses
             // todo: do DROP last, because it cannot be reverted (easily)
             var response = _.reduce(commands,(previous,current) =>
@@ -80,7 +80,7 @@ class History {
                         (success) => {
                             if (success) sequence.pop() // we keep a register of outstanding operations
                             if (source && source.progress) source.progress(1-sequence.length/commands.length)
-                            return $.ajax( { url:endpoint, type:'POST', data:current, contentType:mime} )
+                            return $.ajax( { url:endpoint, type:'POST', data:current.sparql, contentType:mime} )
                         },
                         (failure) => $.Deferred().reject(sequence).promise()
                     ),
@@ -96,8 +96,8 @@ class History {
                     var failed = sequence.slice().reverse()
                     // planned: recover structure of commands
                     this.model.upstream.push(successful)
-                    this.commands = [failed]
-                    this.reset()
+                    var remaining = _.chain(failed).groupBy('i').values().map((v) => v.map((w) => w.sparql)).value()
+                    this.reset(_.concat(this.commands.slice(this.index),remaining))
                 }
             )
             return response
