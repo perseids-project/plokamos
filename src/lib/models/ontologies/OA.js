43cc215e6209fe7a8cc1435551a1b54e12c54bad
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-10-04 09:48:33 -0400

    Move queries into OA.js

--- src/lib/models/ontologies/OA.js
+++ src/lib/models/ontologies/OA.js
@@ -56,6 +56,56 @@ var createMap = {
     "default": () => {} // planned: figure out sane default
 }
 
+var queries = {
+    "byIdentifier": (id) => {
+        var id_phrase = id ? "BIND(<"+id+"> AS ?id)" : "?id rdf:type oa:Annotation ."
+        return [
+            "PREFIX oa: <http://www.w3.org/ns/oa#>",
+            "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>",
+            "SELECT DISTINCT ?id ?g ?s ?p ?o",
+            "WHERE {",
+            id_phrase,
+            "{",
+            "?id oa:hasTarget ?s .",
+            "GRAPH ?g {?s ?p ?o}",
+            "}",
+            "UNION",
+            "{",
+            "?id oa:hasTarget/oa:hasSelector ?s .",
+            "GRAPH ?g {?s ?p ?o}",
+            "}",
+            "UNION",
+            "{",
+            "?id oa:hasBody ?g .",
+            "GRAPH ?g {?s ?p ?o}",
+            "}",
+            "UNION",
+            "{",
+            "?id oa:hasTarget ?t .",
+            "?s oa:hasTarget ?t .",
+            "GRAPH ?g {?s ?p ?o}",
+            "}",
+            "}"
+        ].join("\n")
+    },
+    "http://www.w3.org/ns/oa#hasSelector": (id) => [
+        "SELECT ?id ?g ?s ?p ?o",
+        "WHERE {",
+        "GRAPH ?g {",
+        `${id || "?id"} <http://www.w3.org/ns/oa#hasTarget> ?target .`,
+        "?target <http://www.w3.org/ns/oa#hasSelector> ?s .",
+        "?s ?p ?o",
+        "}}"
+    ].join("\n"),
+    "http://www.w3.org/ns/oa#hasBody": (id) => [
+        "SELECT ?id ?subject ?predicate ?object ?graph",
+        "WHERE {",
+        "GRAPH ?g {?id <http://www.w3.org/ns/oa#hasBody> ?graph } .",
+        "GRAPH ?graph {?subject ?predicate ?object}",
+        "}"
+    ].join("\n")
+}
+
 class OA {
 
     static expand(type) { return expandMap[type] || expandMap.default(type) }
