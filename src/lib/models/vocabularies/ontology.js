b0df1ce854ba3ce44ea3f6832cba5e5f13ebd0db
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-10-07 14:00:31 -0400

    Add class for individual ontologies

---
+++ src/lib/models/vocabularies/ontology.js
@@ -0,0 +1,56 @@
+import $ from 'jquery'
+import _ from 'lodash'
+import Vocabulary from './vocabulary'
+import Transformation from './transformation'
+
+const vocabulary = Symbol()
+const transformation = Symbol()
+
+class Ontology {
+    constructor(voc, trans) {
+        this[vocabulary] = voc
+        this[transformation] = trans
+    }
+
+    // transform:
+
+    simplify(id, graph){
+        this[transformation].simplify(id, graph)
+    }
+
+    expand(gspo, graphs){
+        this[transformation].expand(gspo, graphs)
+    }
+
+    // vocab:
+
+    namespace() {
+        return this[vocabulary].namespace()
+    }
+
+    test(resource) {
+        return _.chain(typeof resource === 'Array' ? resource : [resource]).map(_.values).reduce((result,obj) => result || this[vocabulary].test(obj), false).value()
+    }
+
+    label(resource) {
+        return this[vocabulary].label(resource)
+    }
+
+    unlabel(string) {
+        return this[vocabulary].unlabel(string)
+    }
+
+    description() {
+        //todo: retrieve from endpoint
+    }
+
+    static get(uri) {
+
+        $.when(
+            Vocabulary.get().from(),
+            Transformation.get().from()
+        ).then((voc, trans) => {
+            return new Ontology(voc, trans)
+        })
+    }
+}
