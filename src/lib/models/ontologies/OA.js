4823f3e5ee0243ad16d05a82dff66b1ebed810c1
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-10-18 22:00:27 -0400

    Extend OA.js API

--- src/lib/models/ontologies/OA.js
+++ src/lib/models/ontologies/OA.js
@@ -108,6 +108,31 @@ var queryMap = {
 
 class OA {
 
+    static getBodies(xs) {
+        let ids = xs.filter((x) => (x.p.value || x.p).replace("oa:","http://www.w3.org/ns/oa#") === "http://www.w3.org/ns/oa#hasBody").map((x) => (x.o.value || x.o))
+        let annotation = xs.filter((x) => ((x.o.value || x.o) === "http://www.w3.org/ns/oa#Annotation")).map((x) => (x.s.value || x.s))
+        let partition = _.groupBy(ids,(id) => _.find(xs,(x) => annotation.indexOf(id)+1) ? "graphs" : "resources")
+
+        let result1 = (partition.graphs || []).map((id) => _.filter(xs,(x) => (x.g.value || x.g) === id))
+        let result2 = (partition.resources || []).map((id) => _.filter(xs,(x) => (x.s.value || x.s) != id))
+        return _.concat(result1,result2)
+    }
+
+    static getTargets(xs) {
+        let targetObjs = _.filter(xs,(x) => (x.p.value || x.p).replace("oa:","http://www.w3.org/ns/oa#") === "http://www.w3.org/ns/oa#hasTarget")
+        return targetObjs.map((targetObj) => _.filter(xs, (x) => (x.s.value || x.s) === (targetObj.o.value || targetObj.o)))
+    }
+
+    static getSelectors(xs) {
+        let selectorObjs = _.filter(xs,(x) => (x.p.value || x.p).replace("oa:","http://www.w3.org/ns/oa#") === "http://www.w3.org/ns/oa#hasSelector")
+        return selectorObjs.map((selectorObj) => _.filter(xs, (x) => (x.s.value || x.s) === (selectorObj.o.value || selectorObj.o)))
+    }
+
+    static getAnnotator(xs) {
+        let annotatorObj = _.find(xs,(x) => (x.p.value || x.p).replace("oa:","http://www.w3.org/ns/oa#") === "http://www.w3.org/ns/oa#annotatedBy")
+        return annotatorObj.o.value || annotatorObj.o
+    }
+
     static expand(type) { return expandMap[type] || expandMap.default(type) }
 
     static simplify(type) { return simplifyMap[type] || simplifyMap.default(type) }
