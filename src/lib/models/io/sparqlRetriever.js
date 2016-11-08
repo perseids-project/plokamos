9ce28961ec2c9feff64a8001d451f5b965934fec
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-11-08 11:30:47 -0500

    More broadly compatible Ajax

--- src/lib/models/io/sparqlRetriever.js
+++ src/lib/models/io/sparqlRetriever.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 var sparqlQuery = (endpoint, query, mime) => {
     query = query || "SELECT * WHERE { GRAPH ? {?s ?p ?o}} LIMIT 1000"
     mime = mime || "application/sparql-query"
-    return $.ajax( { url:endpoint, type:'POST', data:query, contentType:mime, dataType:"json" } )
+    return $.ajax( { url:endpoint, method:'POST', data:query, contentType:mime, headers:{ Accept:'application/sparql-results+json'} } )
 }
 
 export default sparqlQuery
