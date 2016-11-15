de3a73dfcad014ddff193661ac20730f256ceda5
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-11-15 12:16:23 -0600

    Add Validator class

--- src/lib/views/annotator/Plugin.js
+++ src/lib/views/annotator/Plugin.js
@@ -189,50 +189,21 @@ class Reporter {
 
 }
 
-class SPARE {
-
-    // todo: hand activate over to View in Subclasses
-     activate(el) {
-    el.find('div.btn-delete').click((e) => {
-        var triple = $(e.target).closest('.triple')
-        triple.animate({'height':'0px', 'margin-top':'0px', 'margin-bottom':'0px' },{duration:150, complete:() =>{$(e.target).closest('.triple').hide()}})
-        triple.addClass('delete')
-        if (!triple.siblings(':not(.delete)').length) triple.closest('.graph.old').addClass('delete')
-    })
-    el.find('#new_button').click((e) => {
-        var split = _.last($("#annotator-main").data('urn').split("."))
-        var character = split ? "http://data.perseus.org/people/smith:"+split.replace(new RegExp('_', 'gi'),"-")+"#this" : ""
-        var triple = $('.graph.new').find('.triple:not(.delete):last')
-        // the following prevents the button from creating a new triple before the previous one has been completed
-        if (!triple.length || (triple.attr('data-subject') && (triple.attr('data-predicate') || triple.attr('data-object')))) {
-            var list = $(Mustache.render("{{> triple}}",Object.assign({},{g:"",s:character,p:"",o:""},self.view),self.partials))
-            list.appendTo($('.graph.new'))
-            activate(list)
+/**
+ *
+ *
+ */
+class Validator {
+    /**
+     *
+     * @param positionsMp A map between token positions and valid namespaces/prefixes
+     * @param validateFn
+     */
+        constructor(positionsMp, validateFn) {
+            this.validate = validateFn(positionsMp)
+            this.map = () => positionsMp
         }
-    })
-    el.find('input').each((i,e) => $(e).typeahead({minLength:3,highlight:true},{source:self.substringMatcher(self.names)}))
-
-    el.find('.token').on('paste',self.updateValue)
-    el.find('.token').on('typeahead:selected',self.updateValue)
-    el.find('.token').on('typeahead:autocompleted', self.updateValue)
-    el.find('.token').on('keyup', (e) => { if (e.key.length===1 || e.key==="Backspace") { self.updateValue(e,e.target.value) }})
-
-    return el
 }
-
-    constructor(){}
-
-    template(){}
-
-    button() {}
-
-    delete_graphs() {}
-    delete_triples() {}
-    update_triples() {}
-    create_triples() {}
-}
-
-class Validator() {}
 
 // todo: develop null ops as placeholders
 // i.e. Plugin is instantiable and does pure gspo handling
