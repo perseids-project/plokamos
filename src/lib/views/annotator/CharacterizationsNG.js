d9b739ad89388785556d6a7fbd165058df7d58a6
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-11-18 13:53:01 -0600

    Add new Characterizations Plugin

---
+++ src/lib/views/annotator/CharacterizationsNG.js
@@ -0,0 +1,10 @@
+import Plugin from './Plugin'
+import OA from '../../models/ontologies/OA'
+import Mustache from 'mustache'
+import _ from 'lodash'
+import $ from 'jquery'
+
+class Characterizations extends Plugin {
+}
+
+export default Characterizations
