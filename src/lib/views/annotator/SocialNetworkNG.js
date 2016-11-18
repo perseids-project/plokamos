7f45621d3913d31c4a316dbf6357098cdfa7e231
Frederik Baumgardt <baumgardt@informatik.uni-leipzig.de>
2016-11-18 13:55:27 -0600

    Add new Social Network Plugin

---
+++ src/lib/views/annotator/SocialNetworkNG.js
@@ -0,0 +1,12 @@
+import Plugin from './Plugin'
+import OA from '../../models/ontologies/OA'
+import Mustache from 'mustache'
+import _ from 'lodash'
+import $ from 'jquery'
+
+class SocialNetwork extends Plugin {
+
+
+}
+
+export default SocialNetwork
